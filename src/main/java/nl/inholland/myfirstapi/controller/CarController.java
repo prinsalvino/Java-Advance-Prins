601ec909ed20b2b6199185e62df064539f3ffb9e
prinsalvino <42965896+prinsalvino@users.noreply.github.com>
2020-05-06 09:34:50 +0200

    coms

--- src/main/java/nl/inholland/myfirstapi/controller/CarController.java
+++ src/main/java/nl/inholland/myfirstapi/controller/CarController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "cars")
+@RequestMapping("cars")
 public class CarController {
 
     @Autowired
